c774eca0a9789e7522fc0da5278025c97751553a
= <ronaldo.alves.rios@gmail.com>
2022-09-13 08:04:10 -0300

    infra do projeto

---
+++ src/main/java/br/com/tcs/SgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeApplication.class, args);
+	}
+
+}
